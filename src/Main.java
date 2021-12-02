300c36bbc3e5f350b35a4dff380a4c3d295397de
Olga Malashenko <olga_birjukova@mail.ru>
2021-12-02 03:24:39 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+   public static void main(String[] args) {
+      int sum;
+      sum = 154;
+      int sumPerMile = 20;
+      int numberOfMiles = sum / sumPerMile;
+
+      System.out.println(numberOfMiles);
+   }
+}
